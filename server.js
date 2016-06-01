36a2b127e68edbedf46b43145bc6a6b7c40f55b2
Aaron Wong <aaronywwong@gmail.com>
2016-06-01 22:01:32 +0930

    Add POST for todos

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Eat food',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
 //a todo is our model
 //a set of todos is called a todo collection
 
+
+//now anytime a json request comes in, express is going to parse it
+//and we are going to be able to access it via req.body
+app.use(bodyParser.json()); 
 
 
 app.get('/', function(req, res){
@@ -46,6 +43,17 @@ app.get('/todos/:id', function(req,res){
 	
 	//res.send('Asking for todo with id of ' + req.params.id);
 });
+
+// POST /todos
+app.post('/todos', function(req, res){
+	var body = req.body;
+	body.id = todoNextId ++;
+	todos.push(body);
+
+	
+	res.json(body);
+})
+
 
 
 app.listen(PORT, function(){
