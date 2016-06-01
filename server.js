c3494b6d760015d46dd4edf4e0736294e9bea206
Aaron Wong <aaronywwong@gmail.com>
2016-06-01 23:00:06 +0930

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -28,12 +28,15 @@ app.get('/todos', function(req,res){
 // GET /todos/:id
 app.get('/todos/:id', function(req,res){
 	var todoId = parseInt(req.params.id, 10); //req.params.id is always a string
-	var matchedTodo;
-	todos.forEach(function(todo){
-		if(todo.id === todoId){
-			matchedTodo = todo;
-		}
-	})
+	
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	// var matchedTodo;
+	// todos.forEach(function(todo){
+	// 	if(todo.id === todoId){
+	// 		matchedTodo = todo;
+	// 	}
+	// })
 
 	if(matchedTodo){
 		res.json(matchedTodo);
@@ -46,8 +49,19 @@ app.get('/todos/:id', function(req,res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body;
+
+	var body = _.pick(req.body, 'description', 'completed');
+
+	// if(typeof body.completed !== 'boolean' || typeof body.description !== 'string' || body.description.trim().length === 0){
+	// 	return res.status(400).send();
+	// }
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId ++;
+
 	todos.push(body);
 
 	
