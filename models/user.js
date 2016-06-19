802bc9a0e55d1dff076008624fda7429086d2d34
Aaron Wong <aaronywwong@gmail.com>
2016-06-19 14:18:41 +0930

    Add auth token generation to POST /users/login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcryptjs');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -57,7 +59,7 @@ module.exports = function(sequelize, DataTypes) {
 							return reject();
 						}
 						resolve(user);
-							}, function(e) {
+					}, function(e) {
 						reject();
 					});
 				});
@@ -67,6 +69,24 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
+			},
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get('id'), type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
