94075023de2490c785587ee6eeaab52f76cec677
Aaron Wong <aaronywwong@gmail.com>
2016-06-22 11:11:51 +0930

    Adding DELETE /users/login

--- middleware.js
+++ middleware.js
@@ -1,14 +1,26 @@
-module.exports = function(db){
+var cryptojs = require('crypto-js');
+module.exports = function(db) {
 	return {
-		requireAuthentication: function(req, res, next){
-			var token = req.get('Auth');
+		requireAuthentication: function(req, res, next) {
+			var token = req.get('Auth') || '';
 
-			db.user.findByToken(token).then(function(user){
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance) {
+				if(!tokenInstance){
+					throw new Error(); //calls catch function
+				}
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+			}).then(function(user){
 				req.user = user;
 				next();
-			}, function(){
+			}).catch(function(){
 				res.status(401).send();
 			});
+
 		}
 	};
 };
